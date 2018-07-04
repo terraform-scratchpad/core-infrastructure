dbfec4bcb5c9c76a635a28b27f17c798ab0472a5
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-07-04 17:10:49 +0200

    Initial Import

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  version = ">=1.8.0"
+}
+
+#
+# creates the key vault to store secrets
+#
+
+module "vault" {
+  source                = "../../modules/vault"
+  location              = "${var.location}"
+  resource_group_name   = "${var.resource_group_name}"
+  tenant_id             = "${var.tenant_id}"
+  object_id             = "${var.object_id}"
+  owner_object_id       = "${var.owner_object_id}"
+}
+
+#
+# create main VMs network
+#
+module "core-network" {
+  source                = "../../modules/network"
+  location              = "${var.location}"
+  resource_group_name   = "${var.resource_group_name}"
+  network_address_space = "172.168.0.0/16"
+  subnet_address_prefix = "172.168.1.0/24"
+}
+
+#
+# store secrets into vault
+#
+
+resource "azurerm_key_vault_secret" "core-network-id" {
+  name = "core-network-id"
+  value = "${module.core-network.core-network-id}"
+  vault_uri = "${module.vault.vault-uri}"
+}
+
+resource "azurerm_key_vault_secret" "core-subnet-01-id" {
+  name = "core-subnet-01-id"
+  value = "${module.core-network.core-subnet-id}"
+  vault_uri = "${module.vault.vault-uri}"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "location" {}
+variable "resource_group_name" {}
+variable "tenant_id" {}
+variable "object_id" {}
+variable "owner_object_id" {}
