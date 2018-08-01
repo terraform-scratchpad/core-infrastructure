b6eb970f62bafb5f0cba714c608d32d28a3937cf
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-01 16:04:38 +0200

    add backend statement

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name  = "lvmhtfstate"
+    container_name        = "tfstate"
+    key                   = "test.core-infrastructure.tfstate"
+  }
+}
+
 provider "azurerm" {
   version = "1.8.0"
 }
