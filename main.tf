6da2e02e70c53da0784fce3d90ee10f9c914ebcd
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-20 16:07:58 +0200

    adapted to vault module update

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "azurerm_resource_group" "tf-resource-group" {
 # creates the key vault to store secrets
 #
 module "vault" {
+  core-vault-name       = "core-vault"
   source                = "github.com/terraform-scratchpad/azure-vault"
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
@@ -53,26 +54,4 @@ module "core-network" {
   network_address_space = "172.168.0.0/16"
   subnet_address_prefix = "172.168.1.0/24"
   tags                  = "${var.tags}"
-}
-
-
-#
-# store secrets into vault
-#
-resource "azurerm_key_vault_secret" "core-network-id" {
-  name      = "core-network-id"
-  value     = "${module.core-network.core-network-id}"
-  vault_uri = "${module.vault.vault-uri}"
-}
-
-resource "azurerm_key_vault_secret" "core-subnet-id" {
-  name      = "core-subnet-01-id"
-  value     = "${module.core-network.core-subnet-id}"
-  vault_uri = "${module.vault.vault-uri}"
-}
-
-resource "azurerm_key_vault_secret" "core-nsg-id" {
-  name      = "core-nsg-id"
-  value     = "${module.core-network.core-nsg-id}"
-  vault_uri = "${module.vault.vault-uri}"
 }
